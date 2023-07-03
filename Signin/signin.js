c6dcf0d3e0821ba00c3d63d9bc41b818fa92cdd2
yagpath <vikasm027@gmail.com>
2023-07-03 14:11:56 +0530

    signin basic structure completed

---
+++ Signin/signin.js
@@ -0,0 +1,12 @@
+function togglePasswordVisibility() {
+  var passwordInput = document.getElementById("password");
+  var passwordToggle = document.getElementById("password-toggle");
+
+  if (passwordInput.type === "password") {
+    passwordInput.type = "text";
+    passwordToggle.textContent = "Hide";
+  } else {
+    passwordInput.type = "password";
+    passwordToggle.textContent = "Show";
+  }
+}
